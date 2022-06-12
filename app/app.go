71f143748dc804f695c6b1bd81fba54c33bf76fe
LJ117 <35555585+LJ117@users.noreply.github.com>
2022-06-12 14:06:18 +0800

    feature: add the go module file
    - style refactor the project struct

---
+++ app/app.go
@@ -0,0 +1,14 @@
+package app
+
+import (
+	"log"
+	"net/http"
+)
+
+func Start() {
+	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
+
+	log.Fatal(http.ListenAndServe("localhost:8888", nil))
+
+}
--- main.go
+++ main.go
@@ -1,51 +1,7 @@
 package main
 
-import (
-	"encoding/json"
-	"encoding/xml"
-	"fmt"
-	"log"
-	"net/http"
-)
-
-type Customer struct {
-	Name    string `json:"full_name" xml:"xml_name"`
-	City    string `json:"city" xml:"xml_city"`
-	ZipCode string `json:"zip_code" xml:"xml_zip_code"`
-}
+import "github.com/seven/banking/app"
 
 func main() {
-
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customers", getAllCustomers)
-
-	log.Fatal(http.ListenAndServe("localhost:8888", nil))
-}
-
-func greet(w http.ResponseWriter, req *http.Request) {
-	fprint, err := fmt.Fprint(w, "Hello World !!!")
-	if err != nil {
-		log.Printf("total %d bytes\n", fprint)
-		return
-	}
-}
-
-func getAllCustomers(w http.ResponseWriter, r *http.Request) {
-	customers := []Customer{
-		{"Seven", "CDC", "610061"},
-		{"Bill", "GD", "710071"},
-	}
-
-	if r.Header.Get("Content-Type") == "application/xml" {
-		w.Header().Set("Content-Type", "application/xml")
-		if err := xml.NewEncoder(w).Encode(customers); err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		w.Header().Set("Content-Type", "application/json")
-		if err := json.NewEncoder(w).Encode(customers); err != nil {
-			log.Fatal(err)
-		}
-	}
-
+	app.Start()
 }
